1210e952572faaf574972ca1d241aaf760bdf392
Zachary Wilson <wilsonze@gmail.com>
2022-12-21 04:20:52 -0600

    BugFix
    Fix for error:
    
    ```python
    >           self.driver = driver_klass(**driver_kwargs)
    E           TypeError: WebDriver.__init__() got an unexpected keyword argument 'firefox_options'
    
    scrapy_selenium/middlewares.py:67: TypeError
    ```

--- scrapy_selenium/__init__.py
+++ scrapy_selenium/__init__.py
@@ -1,2 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .http import SeleniumRequest
 from .middlewares import SeleniumMiddleware
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
